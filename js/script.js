e3263f58d0b76361cde4a41f8b9e27c7ed0502b5
Asana Korede <asanakorededaniel@gmail.com>
2023-11-22 01:54:18 -0800

    create first function and implement in console

---
+++ js/script.js
@@ -0,0 +1,83 @@
+$(document).ready(function () {
+
+    let sentence = prompt("Type your sentence here");
+    var text = sentence;
+
+    //to get the first and last letter of my sentence and turn to a string
+
+    function newString() {
+        let value = text.charAt(0) + text.charAt(text.length - 1);
+        value.toUpperCase();
+        return value.toString();
+    }
+
+
+
+    //to get the first and last letter of my sentence and turn to a string, THEN reverse it
+
+    function reverseNewString() {
+        let reverse = text.charAt(text.length - 1) + text.charAt(0);
+        reverse.toUpperCase();
+        return reverse.toString();
+    }
+
+    //Third Function
+
+    function callFunctions() {
+        newString();
+        reverseNewString();
+        return text + reverseNewString();
+    }
+
+    function countCharacters(){
+        text.length;
+         (text.length)/2;
+        return ind
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
+
